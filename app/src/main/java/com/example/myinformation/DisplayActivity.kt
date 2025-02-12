9b7a4899f40c2ef4be3a1f30c5cf355dd1d0b424
dotjoma <bastejoma8@gmail.com>
2025-02-12 21:23:12 +0800

    Update DisplayActivity

--- app/src/main/java/com/example/myinformation/DisplayActivity.kt
+++ app/src/main/java/com/example/myinformation/DisplayActivity.kt
@@ -1,16 +1,41 @@
 package com.example.myinformation
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class DisplayActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display)
+
+        val fname = intent.getStringExtra("first_name")
+        val lname = intent.getStringExtra("last_name")
+        val age = intent.getStringExtra("age")
+
+        findViewById<TextView>(R.id.tvFname).apply {
+            text = "First Name: $fname"
+        }
+
+        findViewById<TextView>(R.id.tvLname).apply {
+            text = "Last Name: $lname"
+        }
+
+        findViewById<TextView>(R.id.tvAge).apply {
+            text = "Age: $age"
+        }
+
+//        tvFname.text = fname
+//        tvLname.text = lname
+//        tvAge.text = age
     }
 
     public fun returnPage(view: View) {
         onBackPressed()
     }
+
 }
--- app/src/main/java/com/example/myinformation/MainActivity.kt
+++ app/src/main/java/com/example/myinformation/MainActivity.kt
@@ -4,11 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var etFname : EditText
+    private  lateinit var etLname : EditText
+    private lateinit var etAge : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,7 +33,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     public  fun displayInfo(view: View) {
+        etFname = findViewById(R.id.etFname)
+        etLname = findViewById(R.id.etLname)
+        etAge = findViewById(R.id.etAge)
+
+        val fname = etFname.text.toString()
+        val lname = etLname.text.toString()
+        val age = etAge.text.toString()
+
+        if (fname == "" || lname == "" || age == "") {
+            Toast.makeText(this, "Please fill all required fields!", Toast.LENGTH_LONG).show()
+            return
+        }
+
         Intent(this, DisplayActivity::class.java).also {
+            it.putExtra("first_name", fname)
+            it.putExtra("last_name", lname)
+            it.putExtra("age", age)
             startActivity(it)
         }
     }
